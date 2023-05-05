215222822af868ffa9f8dd1466a35b4830c18cdf
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-05-05 12:06:04 +0300

    ContactApp

---
+++ 01/src/Components/contacts/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import List from './List/index.js'
+import Form from './Form/index.js'
+
+function Contacts() {
+  return (
+    <div>
+        <List/>
+        <Form/>
+
+    </div>
+  )
+} 
+
+export default Contacts
